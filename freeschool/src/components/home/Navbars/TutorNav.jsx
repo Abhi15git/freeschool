75ff55ea308b74d06cbb8caa028a50ef9602aed2
Abhishek <9528442373.as@gmail.com>
2021-10-08 21:45:06 +0530

    update

--- freeschool/src/components/home/Navbars/TutorNav.jsx
+++ freeschool/src/components/home/Navbars/TutorNav.jsx
@@ -1,9 +1,25 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from '../../css/Home.module.css'
+
 
 const TutorNav = () => {
     return (
-        <div>
-            tutornav
+        <div className={styles.tutorNavContainer}>
+        <div className={styles.nav}>
+        <div className={styles.logo}>
+        <Link to="/">ele School</Link>
+        </div>
+
+            <div className={styles.links}>
+            <Link to="/">Live Sessions</Link>
+            <Link to="/">Create Session</Link>
+            <Link to="/">Contact Us</Link>
+            <Link to="/">Profile</Link>
+            <Link to="/signin"><Button variant="outlined" size="small" color="inherit">Log out</Button></Link>
+            </div>
+        </div>
         </div>
     )
 }
--- freeschool/src/components/home/TutorHome.jsx
+++ freeschool/src/components/home/TutorHome.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const TutorHome = () => {
+    let arr = [
+        {
+            class:'6th',
+            subject:'maths',
+            time:'9:30 pm',
+            link:'this is a link'
+        },
+        {
+            class:'8th',
+            subject:'science',
+            time:'9:00 pm',
+            link:'this is a link'
+        }
+    ]
     return (
         <div>
-            tutorHome
+            
+            
             
         </div>
     )
