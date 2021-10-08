195c31ba6dab562767a88cc2a3635383be7b0c29
ravishukla86044 <ravishukls86044@gmail.com>
2021-10-08 20:08:25 +0530

    student singup done

--- freeschool/src/components/LoginAndSignup/StudentSignup.jsx
+++ freeschool/src/components/LoginAndSignup/StudentSignup.jsx
@@ -17,16 +17,40 @@ export default function StudentSignup() {
 
   const onEnter = (e) => {
     const { name, value } = e.target;
-
+    if (name === "name") {
+      setError({ ...error, name: false });
+    }
+    if (name === "email") {
+      setError({ ...error, email: false });
+    }
+    if (name === "password") {
+      setError({ ...error, password: false });
+    }
     setForm({ ...form, [name]: value });
   };
 
+  const validateForm = () => {
+    if (form?.email === "" && form?.password === "" && form.name === "")
+      setError({ email: "Email is required", password: "Password is required" });
+    else if (form?.name === "") setError({ error, name: "Name is required" });
+    else if (form?.email === "") setError({ error, email: "Email is required" });
+    else if (form?.password === "") setError({ error, password: "Password is required" });
+    else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(form?.email))
+      setError({ error, email: "Invalid email" });
+    else if (form?.password.length < 8)
+      setError({ ...error, password: "Password must be atleast 8 characters long" });
+  };
+
   const handelSubmit = (e) => {
-    console.log("1");
+    validateForm();
+    if (error.email === false && error.password === false && error.name === false) {
+      console.log("1");
+    }
   };
 
   return (
     <Sin>
+      <h3>Student Signup</h3>
       <div>
         <TextField
           id="outlined-basic"
@@ -76,10 +100,37 @@ const Sin = styled.div`
   display: flex;
   flex-direction: column;
   background: #ffffff;
-  width: 50%;
-  height: 100%;
+  padding: 20px;
+  width: 60%;
+
+  height: max-content;
   box-sizing: border-box;
   box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034), 0 6.7px 5.3px rgba(0, 0, 0, 0.048),
     0 12.5px 10px rgba(0, 0, 0, 0.06), 0 22.3px 17.9px rgba(0, 0, 0, 0.072),
     0 41.8px 33.4px rgba(0, 0, 0, 0.086), 0 100px 80px rgba(0, 0, 0, 0.12);
+  & > div {
+    margin-top: 40px;
+    position: relative;
+  }
+  & .MuiFormControl-root {
+    width: 100%;
+    min-width: 300px;
+  }
+  & .MuiOutlinedInput-root {
+    border-radius: 50px;
+  }
+
+  & .MuiFormHelperText-contained {
+    position: absolute;
+    bottom: -20px;
+  }
+  .signSubmit {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+  }
+  .signSubmit .MuiButton-containedPrimary {
+    border-radius: 50px;
+    background-color: #189d0e;
+  }
 `;
