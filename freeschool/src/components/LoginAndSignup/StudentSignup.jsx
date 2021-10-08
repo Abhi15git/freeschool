b0a60919559fb01d0a7bb7b431f5d887ae5c496c
ravishukla86044 <ravishukls86044@gmail.com>
2021-10-08 19:55:21 +0530

    basic signup for student

--- freeschool/src/components/LoginAndSignup/StudentSignup.jsx
+++ freeschool/src/components/LoginAndSignup/StudentSignup.jsx
@@ -1,7 +1,75 @@
 import styled from "styled-components";
+import { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export default function StudentSignup() {
-  return <Sin></Sin>;
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState({
+    name: false,
+    email: false,
+    password: false,
+  });
+
+  const onEnter = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  const handelSubmit = (e) => {
+    console.log("1");
+  };
+
+  return (
+    <Sin>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          onKeyUp={onEnter}
+          name="name"
+          required
+          error={error.name}
+          helperText={error.name}
+        />
+      </div>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+          onKeyUp={onEnter}
+          name="email"
+          required
+          error={error.email}
+          helperText={error.email}
+        />
+      </div>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          onKeyUp={onEnter}
+          name="password"
+          required
+          error={error.password}
+          helperText={error.password}
+        />
+      </div>
+      <div className="signSubmit">
+        <Button variant="contained" color="primary" onClick={handelSubmit}>
+          Signup
+        </Button>
+      </div>
+    </Sin>
+  );
 }
 
 const Sin = styled.div`
