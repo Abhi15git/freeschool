7ca6fc963f4936981ed852d5d4a38989f82a9665
ravishukla86044 <ravishukls86044@gmail.com>
2021-10-09 09:18:38 +0530

    pull requet

--- freeschool/src/App.js
+++ freeschool/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Routes from "./components/home/Routes";
-import StudentBoth from "./components/LoginAndSignup/StudentBoth";
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes /> */}
-      <StudentBoth />
+      <Routes />
     </div>
   );
 }
