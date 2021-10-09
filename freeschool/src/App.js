fd7b02c8eb121388af9c28dafd3e6028284fb852
ravishukla86044 <ravishukls86044@gmail.com>
2021-10-09 14:03:10 +0530

    remove extrass

--- freeschool/src/App.js
+++ freeschool/src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 import Routes from "./components/home/Routes";
 
-import { useState } from "react";
-
 function App() {
-  const [start, setStart] = useState(false);
-
   return (
     <div className="App">
       <Routes />
